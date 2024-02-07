9fc72e9da448d303b7fa2dcd82ff81d0951a9fce
abrar0349 <abrar0349khan@gmail.com>
2024-02-07 11:25:33 +0500

    Python Today is my practice day 2

---
+++ 02_basic.py
@@ -0,0 +1,13 @@
+# file ka name No sy Shoro nhi ho sakta h isly first litter alphabit ho
+from basic_01 import khan
+
+khan(5)
+
+# jab import krky koi khan file ma sy isty mal krty h to ak file bnti h __pycache__ ye hr bar bnti ha magar import ky waqat hamy show bhi hoti h 
+
+# Ma ny as Module ko bhi import kiya or check kiya Os module ky sath aj khyla ho
+
+import os
+os.getcwd()
+os.listdir(path='C:\\Users\\MS Office\\Desktop\\khan')
+# uper wala code python ky shell ma run hota h or many is my istimal kiya h
