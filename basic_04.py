6dfeeaa9471e26d05d115b25c45bf2ba0dca73a8
abrar0349 <abrar0349khan@gmail.com>
2024-02-18 11:13:08 +0500

    Today I liter bit work on str

--- basic_04.py
+++ basic_04.py
@@ -1,6 +1,14 @@
-import calendar
-# print(calendar.month(2023,12))
+# import calendar
+# # print(calendar.month(2023,12))
 
 
-import random
-print(random.randint(1,100))
+# import random
+# print(random.randint(1,100))
+
+
+str = 'Hello World'
+
+print(str[0::2])
+print(str.upper())
+print(str.lower())
+
